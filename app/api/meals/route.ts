2d38e1156c5d88e1c67cd5ccab3a0727fb676b84
Harsh Gaurav <gauravharsh.1996@gmail.com>
2026-05-22 01:08:51 +0530

    fix api route structure

--- app/api/meals/route.ts
+++ app/api/meals/route.ts
@@ -1,22 +1,10 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  try {
-    const body = await req.json();
+  const body = await req.json();
 
-    console.log("BODY:", body);
-
-    return NextResponse.json({
-      success: true,
-      received: body,
-    });
-
-  } catch (error) {
-    console.error(error);
-
-    return NextResponse.json(
-      { error: "API crashed" },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json({
+    success: true,
+    body,
+  });
 }
